ff0787186affd8e84217815c2849bc4b47603bf1
pedro7161 <46382185+pedro7161@users.noreply.github.com>
2021-10-20 09:38:35 +0100

    First snippet

---
+++ code snippets/a/aApp.swift
@@ -0,0 +1,17 @@
+//
+//  aApp.swift
+//  a
+//
+//  Created by Aluno Istec on 20/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
